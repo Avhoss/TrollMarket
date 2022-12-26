68f9044d15ab3be717fb04fb9fbced24ebfb2fe9
unknown <morgan.jixk@gmail.com>
2022-12-26 16:11:28 +0700

    fixed utilities capitalize fucntion

--- src/main/java/com/trollmarket/utility/Utilities.java
+++ src/main/java/com/trollmarket/utility/Utilities.java
@@ -4,6 +4,6 @@ public class Utilities {
 
     public static String capitalizeFirstChar(String theWord){
 
-        return theWord.substring(0, 1).toUpperCase() + theWord.substring(1, theWord.length());
+        return theWord.substring(0, 1).toUpperCase() + theWord.substring(1, theWord.length()).toLowerCase();
     }
 }
